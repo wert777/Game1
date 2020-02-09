af5fc3059870def702224d48553843beab4c455c
wert777 <nickgryzenkov@yandex.ru>
2020-02-09 13:46:23 +1000

    саня

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def face(color):
     pygame.draw.arc(player, (10, 10, 10), (5, 19, 30, 20), 3.1, 6.0, 4)
 
 
-player_rect = player.get_rect(center=(WIN_WIDTH // 2,WIN_HEIGHT // 2))
+player_rect = player.get_rect(center=(WIN_WIDTH // 2, WIN_HEIGHT // 2))
 
 text = pygame.font.SysFont("Arial", 22, True, False)
 text_xy = ((WIN_WIDTH - text.size(f"Штрафных очков {round(penalty, 1)}")[0]) // 2, 30)
@@ -79,7 +79,16 @@ while run:
     for e in pygame.event.get():
         if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
             run = False
-
+        elif e.type == pygame.MOUSEBUTTONDOWN:
+            if e.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+                if (
+                    mouse_pos[0] >= (WIN_WIDTH - BTN_W) // 2
+                    and mouse_pos[0] <= (WIN_WIDTH + BTN_W) // 2
+                    and mouse_pos[1] >= WIN_HEIGHT // 2
+                    and mouse_pos[1] <= WIN_HEIGHT // 2 + BTN_H
+                ):
+                    print(1111)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
         player_rect.x += PLAYER_SPEED
@@ -96,6 +105,9 @@ while run:
     else:
         if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
             player_rect.x += PLAYER_SPEED
+    
+    color = BLUE
+    face(color)
 
     x = dx
     y = 0
@@ -105,11 +117,11 @@ while run:
                 brick = pygame.draw.rect(screen, BRICK_COLOR, [x, y, BRICK_WIDTH, BRICK_HEIGHT])
                 pygame.draw.rect(screen, BRICK_COLOR_2, [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
                 if brick.colliderect(player_rect):
-                    if color == BLUE:
-                        color = RED
-                        face(color)
-                    face(RED)
-                    penalty += 0.1
+                    if player_rect.x < WIN_WIDTH - PLAYER_SIZE * 2:
+                        if color == BLUE:
+                            color = RED
+                            face(color)
+                        penalty += 0.1
             x += BRICK_WIDTH
         y += BRICK_HEIGHT
         x = dx
